00e0720a2c5ad06559fe3396bb6824d0101b09ce
Rander Oliveira <rander.oliveira@escolaemmovimento.com.br>
2021-04-11 20:48:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using KafkaClient;
+using System;
+
+namespace KafkaChat
+{
+    class Program
+    {
+        static bool ProccessMessage(string message)
+        {
+            Console.WriteLine($"Received: {message}");
+            return true;
+        }
+
+        static async void Loop(KafkaClientAPI kafkaClient)
+        {
+            Console.WriteLine("Listening to messages...");
+            await kafkaClient.SubscribeAsync<string>("chat", (message) => ProccessMessage(message));
+            while (true)
+            {
+                Console.WriteLine("Type your message!");
+                var message = Console.ReadLine();
+                await kafkaClient.PublishAsync("chat", message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Loop(new KafkaClientAPI());
+        }
+    }
+}
